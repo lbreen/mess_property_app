10c7bc371b4b6daa33b864e10e42186b335cc016
Luke Breen <l.breen1@outlook.com>
2018-09-20 18:26:16 +0300

    User can view items#index and items#show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,21 @@ class ItemsController < ApplicationController
     @items = policy_scope(Item)
   end
 
+  def show
+    authorize @item
+  end
+
   def new
+    @item = Item.new
   end
 
   def create
+    item = Item.new(item_params)
+    item.user = current_user
+    if item.save
+      redirect_to item_path(item)
+    end
+
   end
 
   def edit
@@ -24,5 +35,9 @@ class ItemsController < ApplicationController
 
   def find_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :location, :reference_number, :comments)
   end
 end
