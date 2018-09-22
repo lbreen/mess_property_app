8bfed99841f1fe8df572c20b61a2712f5a5cc439
Luke Breen <l.breen1@outlook.com>
2018-09-22 16:15:01 +0300

    Each item-photo now appears in a fullscreen modal. ISSUE: Modal css clashes with the item transformation css resulting in the modal flickering

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,9 +1,11 @@
 module ItemHelper
-  def display_item_photo(item)
+  def display_item_photo(item, attr = {})
+    classes = attr[:classes] || ''
+
     if item.photo?
-      cl_image_tag @item.photo, height: 400, width: 400
+      cl_image_tag item.photo, class: "#{classes}"
     else
-      image_tag "no_image_available"
+      image_tag "no_image_available", class: "#{classes}"
     end
   end
 end
